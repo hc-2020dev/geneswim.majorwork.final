1356cf31e1081cbe22436810cd5133f83b602c46
hc-2020dev <harry.chow1@education.nsw.gov.au>
2025-07-24 19:19:26 +1000

    Connection to localhost

--- Homepage/homepage.php
+++ Homepage/homepage.php
@@ -95,51 +95,7 @@
         </div>
     </section>
 
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="grid">
-                <div class="col-6_md-12">
-                    <div class="footer-brand">
-                        <img src="https://ext.same-assets.com/4193966861/740545503.svg" alt="GeneSwim Logo" class="footer-logo">
-                        <span class="footer-brand-text">GeneSwim</span>
-                    </div>
-                    <div class="social-links">
-                        <a href="#" class="social-link">
-                            <img src="https://ext.same-assets.com/4193966861/1164539889.png" alt="Instagram">
-                        </a>
-                        <a href="#" class="social-link">
-                            <img src="https://ext.same-assets.com/4193966861/231688416.png" alt="Facebook">
-                        </a>
-                        <a href="#" class="social-link">
-                            <img src="https://ext.same-assets.com/4193966861/2926978406.png" alt="YouTube">
-                        </a>
-                    </div>
-                    <div class="newsletter">
-                        <h3 class="newsletter-title">Subscribe to our newsletter • Don't miss out!</h3>
-                        <form class="newsletter-form" action="subscribe.php" method="POST">
-                            <input type="email" name="email" placeholder="Email" class="newsletter-input" required>
-                            <button type="submit" class="btn-newsletter">Join</button>
-                        </form>
-                        <label class="newsletter-checkbox">
-                            <input type="checkbox" name="subscribe" checked>
-                            <span>I want to subscribe to your mailing list.</span>
-                        </label>
-                    </div>
-                </div>
-                <div class="col-6_md-12">
-                    <div class="footer-links">
-                        <a href="#" class="footer-link">About</a>
-                        <a href="#" class="footer-link">FAQ</a>
-                        <a href="contact.php" class="footer-link">Contact</a>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-                <p>&copy; 2023 by GeneSwim. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
+<?php require_once 'footer.php'; ?>
 
     <script src="script.js"></script>
 </body>
